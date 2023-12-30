cb6f77d139bb038939273e7d795ea4e44d5ba4ac
Grigory Tsuryev <tsurievga@gmail.com>
2023-12-30 23:36:36 +0200

    s

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,51 +1,83 @@
 import unittest
-from employees import get_employee, employee_is_valid, create_employee, update_employee, delete_employee
+import json
+from employees import app, employees, get_employee, employee_is_valid
+from unittest.mock import patch
 
 class TestEmployeeFunctions(unittest.TestCase):
 
-    def setUp(self):
-        self.employees = [
-            {'id': 1, 'name': 'Ashley'},
-            {'id': 2, 'name': 'Kate'},
-            {'id': 3, 'name': 'Joe'}
-        ]
+        def setUp(self):
+            self.app = app.test_client()
+            self.employees = [
+                {'id': 1, 'name': 'Ashley'},
+                {'id': 2, 'name': 'Kate'},
+                {'id': 3, 'name': 'Joe'}
+            ]
+        
+        def test_get_employees(self):
+            response = self.app.get('/employees')
+            self.assertEqual(response.status_code, 200)
+            data = json.loads(response.get_data(as_text=True))
+            self.assertEqual(data, employees)
+        
+        def test_get_employee_by_id(self):
+            response = self.app.get('/employees/1')
+            self.assertEqual(response.status_code, 200)
+            data = json.loads(response.get_data(as_text=True))
+            self.assertEqual(data, self.employees[0])
 
-    def test_get_employee_by_id_existing(self):
-        employee = get_employee(1, self.employees)
-        self.assertIsNotNone(employee)
-        self.assertEqual(employee['id'], 1)
-        self.assertEqual(employee['name'], 'Ashley')
+            response = self.app.get('/employees/5')
+            self.assertEqual(response.status_code, 404)
+            data = json.loads(response.get_data(as_text=True))
+            self.assertEqual(data['error'], 'Employee does not exist')
 
-    def test_get_employee_by_id_non_existing(self):
-        employee = get_employee(4, self.employees)
-        self.assertIsNone(employee)
+        @patch('employees.employees', [{'id': 1, 'name': 'Ashley'}])
+        def test_employee_is_valid(self):
+            valid_employee = {'name': 'John'}
+            invalid_employee = {'id': 2, 'name': 'Kate'}
+            self.assertTrue(employee_is_valid(valid_employee))
+            self.assertFalse(employee_is_valid(invalid_employee))
 
-    def test_employee_is_valid_valid_employee(self):
-        valid_employee = {'name': 'John'}
-        self.assertTrue(employee_is_valid(valid_employee))
+        def test_create_employee(self):
+            new_employee = {'name': 'Sophia'}
+            response = self.app.post('/employees', data=json.dumps(new_employee), content_type='application/json')
+            self.assertEqual(response.status_code, 201)
+            self.assertIn('/employees/', response.headers['Location'])
 
-    def test_employee_is_valid_invalid_employee(self):
-        invalid_employee = {'name': 'Alex', 'age': 25}
-        self.assertFalse(employee_is_valid(invalid_employee))
+            created_employee_id = int(response.headers['Location'].split('/')[-1])
+            created_employee = get_employee(created_employee_id)
+            self.assertIsNotNone(created_employee)
+            self.assertEqual(created_employee['name'], new_employee['name'])
 
-    def test_create_employee(self):
-        next_id = len(self.employees) + 1
-        new_employee = {'name': 'Michael', 'id': next_id}
-        create_employee(self.employees, new_employee)
-        self.assertEqual(len(self.employees), 4)
-        self.assertIn(new_employee, self.employees)
+        def test_update_employee(self):
+            employee_id = 1
+            updated_employee_data = {'name': 'Updated Name'}
+            response = self.app.put(f'/employees/{employee_id}', data=json.dumps(updated_employee_data), content_type='application/json')
+            self.assertEqual(response.status_code, 200)
 
-    def test_update_employee(self):
-        updated_info = {'name': 'Updated Name'}
-        update_employee(1, updated_info, self.employees)
-        updated_employee = get_employee(1, self.employees)
-        self.assertEqual(updated_employee['name'], 'Updated Name')
+            updated_employee = get_employee(employee_id)
+            self.assertEqual(updated_employee['name'], updated_employee_data['name'])
 
-    def test_delete_employee(self):
-        delete_employee(1, self.employees)
-        deleted_employee = get_employee(1, self.employees)
-        self.assertIsNone(deleted_employee)
-        self.assertEqual(len(self.employees), 2)
+            response = self.app.put('/employees/10', data=json.dumps(updated_employee_data), content_type='application/json')
+            self.assertEqual(response.status_code, 404)
+
+        def test_delete_employee(self):
+            employee_id = 1
+            response = self.app.delete(f'/employees/{employee_id}')
+            self.assertEqual(response.status_code, 200)
+
+            deleted_employee = get_employee(employee_id)
+            self.assertIsNone(deleted_employee)
+
+            response = self.app.delete('/employees/10')
+            self.assertEqual(response.status_code, 404)
+
+        def test_invalid_employee_properties(self):
+            invalid_employee = {'id': 5, 'name': 'Invalid', 'age': 30}
+            response = self.app.post('/employees', data=json.dumps(invalid_employee), content_type='application/json')
+            self.assertEqual(response.status_code, 400)
+
+            response = self.app.put('/employees/1', data=json.dumps(invalid_employee), content_type='application/json')
+            self.assertEqual(response.status_code, 400)
 
 if __name__ == '__main__':
     unittest.main()
